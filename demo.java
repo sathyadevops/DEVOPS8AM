af87cec224df0e8dc0dfda42f8f8e83ca766f9d6
sathyadevops <sathyadevops1@gmail.com>
2024-05-07 03:10:22 +0000

    new java code

---
+++ demo.java
@@ -0,0 +1,10 @@
+class Demo
+{
+   public static void main(String []args)
+   {
+      System.out.println(" Test repo ");
+      System.out.println(" Test repo ");
+      System.out.println(" Test repo ");
+      System.out.println(" Test repo ");
+   }
+}
