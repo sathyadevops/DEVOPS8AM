5b7caaf484d7d27ffc06a3e2823fd6e1a27c3398
root <root@ip-172-31-47-199.us-east-2.compute.internal>
2024-05-08 02:56:16 +0000

    java code

---
+++ demo.java
@@ -0,0 +1,12 @@
+class Demo
+{
+  public static void main(String []args)
+  {
+    System.out.print(" Test Program ");
+    System.out.print(" Test Program ");
+    System.out.print(" Test Program ");
+    System.out.print(" Test Program ");
+    System.out.print(" Test Program ");
+  }
+   
+}
