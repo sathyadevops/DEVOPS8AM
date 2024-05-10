d0bacfea63692da8173d1597c0e7039092e74734
sathyadevops <sathyadevops1@gmail.com>
2024-05-10 02:56:03 +0000

    add(), sub() code

--- demo.java
+++ demo.java
@@ -1,3 +1,27 @@
+
+
+
+class ArithOPerations
+{
+   void Addition()
+   {
+     c=a+b
+   }
+
+   void Subtraction()
+   {
+     c=a-b
+   }
+
+}
+
+
+
+
+
+
+
+
 class Demo
 {
   public static void main(String []args)
